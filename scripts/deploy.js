3f130ba90dac8b5bcde5cd1f507ec2f6348a3828
Gesora Ian <gishnora@gmail.com>
2023-12-03 19:43:03 +0300

    1

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,29 @@
 const hre = require("hardhat");
 
-async function main() {}
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const TokenFaucet = await hre.ethers.getContractFactory("TokenFaucet");
+  const tokenFaucet = await TokenFaucet.deploy(
+    "TokenFaucet", // _name
+    "TF", // _symbol
+    1000000, // _initialSupply
+    1000, // _requestLimit
+    60 // _requestCooldown
+  );
+
+  await tokenFaucet.deployed();
+
+  console.log("TokenFaucet contract address:", tokenFaucet.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- test/Faucet-test.js
+++ test/Faucet-test.js
@@ -1,55 +1,141 @@
 const { expect } = require("chai");
-//const { ethers } = require("hardhat");
-//const hre = require("hardhat");
-const {
-  loadFixture,
-} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
-// Test suite for the TokenFaucet contract
-describe("Token Faucet", function () {
-  console.log("Faucet Ready for: ");
-  async function deployFaucetFixture() {
-    // Initial values for testing
-    const initialSupply = 1000000;
-    const requestLimit = 100;
-    const requestCooldown = 60;
+describe("TokenFaucet contract", function () {
+  let TokenFaucet;
+  let hardhatTokenFaucet;
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
 
-    // default signer
-    const [signer, user1, user2] = await ethers.getSigners();
+  beforeEach(async function () {
+    TokenFaucet = await ethers.getContractFactory("TokenFaucet");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
 
-    const TokenFaucet = await ethers.getContractFactory("TokenFaucet");
-    const tokenFaucet = await TokenFaucet.deploy(
-      initialSupply,
-      requestLimit,
-      requestCooldown,
-      { from: signer }
+    hardhatTokenFaucet = await TokenFaucet.deploy(
+      "TokenFaucet",
+      "TF",
+      1000000,
+      1000,
+      60
     );
 
-    //await tokenFaucet.waitForDeployment();
+    await hardhatTokenFaucet.waitForDeployment();
+    console.log(
+      "HardhatTokenFaucet Contract Address:",
+      await hardhatTokenFaucet.getAddress()
+    );
+  });
 
-    return {
-      tokenFaucet,
-      signer,
-      user1,
-      user2,
-      initialSupply,
-      requestLimit,
-      requestCooldown,
-    };
-  }
   describe("Deployment", function () {
-    //test #1
-    it("Should set the right owner of the faucet", async function () {
-      const { tokenFaucet, signer } = await loadFixture(deployFaucetFixture);
-      expect(await tokenFaucet.owner()).to.equal(signer.address);
+    it("Should set the right owner", async function () {
+      expect(await hardhatTokenFaucet.owner()).to.equal(owner.address);
     });
-    //test#2
-    it("should set initial supply greater than request limit and assign owner", async function () {
-      const { tokenFaucet, signer } = await loadFixture(deployFaucetFixture);
-      const initialSupply = await tokenFaucet.balanceOf(signer.address);
-      expect(await tokenFaucet.requestTokens()).to.be.lessThan(initialSupply);
+
+    it("Should assign the total supply of tokens to the owner", async function () {
+      const ownerBalance = await hardhatTokenFaucet.balanceOf(owner.address);
+      expect(await hardhatTokenFaucet.totalSupply()).to.equal(ownerBalance);
     });
   });
 
-  // Additional test cases can be added to cover edge cases, security checks, and additional features.
+  describe("Token request", function () {
+    it("Should transfer tokens to the user", async function () {
+      const initialOwnerBalance = await hardhatTokenFaucet.balanceOf(
+        owner.address
+      );
+      const initialContractBalance = await hardhatTokenFaucet.balanceOf(
+        hardhatTokenFaucet.address
+      );
+
+      await hardhatTokenFaucet.connect(addr1).requestTokens();
+
+      const finalOwnerBalance = await hardhatTokenFaucet.balanceOf(
+        owner.address
+      );
+      const finalContractBalance = await hardhatTokenFaucet.balanceOf(
+        hardhatTokenFaucet.address
+      );
+
+      expect(finalOwnerBalance).to.equal(initialOwnerBalance.sub(1000));
+      expect(finalContractBalance).to.equal(initialContractBalance.add(1000));
+    });
+
+    it("Should update the last request timestamp", async function () {
+      await hardhatTokenFaucet.connect(addr1).requestTokens();
+
+      const lastRequestTimestamp =
+        await hardhatTokenFaucet.lastRequestTimestamp(addr1.address);
+      expect(lastRequestTimestamp).to.be.gt(0);
+    });
+
+    it("Should emit an event when tokens are requested", async function () {
+      await expect(hardhatTokenFaucet.connect(addr1).requestTokens())
+        .to.emit(hardhatTokenFaucet, "TokensRequested")
+        .withArgs(addr1.address, 1000);
+    });
+
+    it("Should not allow a user to request tokens within the cooldown period", async function () {
+      await hardhatTokenFaucet.connect(addr1).requestTokens();
+      await hardhatTokenFaucet.connect(addr1).requestTokens();
+    });
+
+    it("Should not allow a user to request tokens if the faucet has insufficient tokens", async function () {
+      await hardhatTokenFaucet.connect(owner).setRequestLimit(1000001);
+      await hardhatTokenFaucet.connect(addr1).requestTokens();
+    });
+  });
+
+  describe("Owner-only functions", function () {
+    it("Should set a new request limit", async function () {
+      await hardhatTokenFaucet.connect(owner).setRequestLimit(2000);
+      expect(await hardhatTokenFaucet.requestLimit()).to.equal(2000);
+    });
+
+    it("Should set a new request cooldown period", async function () {
+      await hardhatTokenFaucet.connect(owner).setRequestCooldown(120);
+      expect(await hardhatTokenFaucet.requestCooldown()).to.equal(120);
+    });
+
+    it("Should not allow a non-owner to set a new request limit", async function () {
+      await expect(
+        hardhatTokenFaucet.connect(addr1).setRequestLimit(2000)
+      ).to.be.revertedWith("Not the owner");
+    });
+
+    it("Should not allow a non-owner to set a new request cooldown period", async function () {
+      await expect(
+        hardhatTokenFaucet.connect(addr1).setRequestCooldown(120)
+      ).to.be.revertedWith("Not the owner");
+    });
+  });
+
+  describe("Edge cases", function () {
+    it("Should not allow a user to request 0 tokens", async function () {
+      await hardhatTokenFaucet.connect(owner).setRequestLimit(0);
+      await expect(
+        hardhatTokenFaucet.connect(addr1).requestTokens()
+      ).to.be.revertedWith("Not enough tokens in the faucet");
+    });
+
+    it("Should not allow a user to request more tokens than the total supply", async function () {
+      await hardhatTokenFaucet.connect(owner).setRequestLimit(1000001);
+      await expect(
+        hardhatTokenFaucet.connect(addr1).requestTokens()
+      ).to.be.revertedWith("Not enough tokens in the faucet");
+    });
+
+    it("Should not allow the owner to set a request limit greater than the total supply", async function () {
+      await expect(
+        hardhatTokenFaucet.connect(owner).setRequestLimit(1000001)
+      ).to.be.revertedWith("Request limit must be less than the total supply");
+    });
+
+    it("Should not allow the owner to set a request cooldown period of 0", async function () {
+      await expect(
+        hardhatTokenFaucet.connect(owner).setRequestCooldown(0)
+      ).to.be.revertedWith("Cooldown period must be greater than 0");
+    });
+  });
 });
